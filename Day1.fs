448e4b6e5a4e049b551642a0dcca0b8b5f375570
N1kica <68340084+N1kica@users.noreply.github.com>
2023-12-07 08:16:20 +0100

    Update Day1.fs

--- Day1.fs
+++ Day1.fs
@@ -6,16 +6,16 @@ let rec find_number (line: string) index direction =
         None
     else
         match line.Substring(index) with
-        | c when System.Char.IsDigit(c.[0]) -> Some(c.[0])
-        | s when s.StartsWith "one" -> Some('1')
-        | s when s.StartsWith "two" -> Some('2')
-        | s when s.StartsWith "three" -> Some('3')
-        | s when s.StartsWith "four" -> Some('4')
-        | s when s.StartsWith "five" -> Some('5')
-        | s when s.StartsWith "six" -> Some('6')
-        | s when s.StartsWith "seven" -> Some('7')
-        | s when s.StartsWith "eight" -> Some('8')
-        | s when s.StartsWith "nine" -> Some('9')
+        | c when System.Char.IsDigit(c.[0]) -> Some(int c.[0] - int '0')
+        | s when s.StartsWith "one" -> Some(1)
+        | s when s.StartsWith "two" -> Some(2)
+        | s when s.StartsWith "three" -> Some(3)
+        | s when s.StartsWith "four" -> Some(4)
+        | s when s.StartsWith "five" -> Some(5)
+        | s when s.StartsWith "six" -> Some(6)
+        | s when s.StartsWith "seven" -> Some(7)
+        | s when s.StartsWith "eight" -> Some(8)
+        | s when s.StartsWith "nine" -> Some(9)
         | _ -> find_number line (index + direction) direction
 
 let rec sum_lines sum = 
@@ -24,12 +24,12 @@ let rec sum_lines sum =
     | line ->
         let first_digit =
             match find_number line 0 1 with
-            | Some(digit) -> int digit - int '0'
+            | Some(digit) -> digit
             | None -> 0
         
         let last_digit =
             match find_number line (line.Length - 1) (-1) with
-            | Some(digit) -> int digit - int '0'
+            | Some(digit) -> digit
             | None -> 0
 
         sum_lines sum + first_digit * 10 + last_digit
