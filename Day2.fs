f547460ca34d95065974ea93dc0b83446bf0fde4
N1kica <68340084+N1kica@users.noreply.github.com>
2023-12-13 10:00:10 +0100

    Update Day2.fs

--- Day2.fs
+++ Day2.fs
@@ -12,29 +12,27 @@ let exceeds_max count color =
     | "blue" -> count > max_blue
     | _ -> false
 
-let get_game (line: string) =
-    let start = line.IndexOf(' ')
-    let end_idx = line.IndexOf(':')
-    let game_id = line.Substring(start, end_idx - start)
-    printfn "Start %s" game_id
-    0
+let rec parse_round (input: string array) index sum game=
+    match exceeds_max (int input.[index]) input.[index + 1] with
+    | true -> sum 
+    | _ ->  
+        if index + 2 >= input.Length then
+            sum + game
+        else
+            parse_round input (index + 2) sum game
 
-let rec get_rounds (line: string) =
-    let round_end = line.IndexOf(';')
-    if round_end > 0 then
-        printfn "Round: %s" (line.Substring(0, round_end))
-        get_rounds (line.Substring(round_end + 2))
-    else 
-        printfn "Round: %s" (line.Substring(0))
-        0
-
-let rec sum_games sum = 
+let rec sum_games sum game = 
     match System.Console.ReadLine() with  
     | null | "" -> sum
     | line -> 
-        printfn "---Game---"
-        let round = get_rounds (line.Substring(line.IndexOf(':') + 2))
-        sum_games sum + 1
+        let round = 
+            parse_round (line.Substring(line.IndexOf(':') + 2)
+                .Trim()
+                .Replace(",", "")
+                .Replace(";", "")
+                .Split([|' '|], System.StringSplitOptions.RemoveEmptyEntries)
+            ) 0 sum game
+        sum_games round (game + 1)
 
-let total_sum = sum_games 0
-printfn "Total sum: %d" total_sum
+let result  = sum_games 0 1
+printfn "Total sum: %d" result
