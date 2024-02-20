d6abe0fe9417c08a5599ac903c7c13ec2897dd7a
N1kica <68340084+N1kica@users.noreply.github.com>
2024-02-20 15:22:05 +0100

    Update Day4.rs

--- Day4.rs
+++ Day4.rs
@@ -1,22 +1,7 @@
 use std::fs;
 
 fn main() {
-    let points1 = fs::read_to_string("input.txt")
-        .expect("Something went wrong reading file!")
-        .lines()
-        .filter_map(|line| line
-            .split_once(":")
-            .and_then(|(_, second)| second.split_once("|"))
-        )
-        .map(|(first, second)| (
-            first.split_whitespace().filter_map(|s| s.parse().ok()).collect::<Vec<i32>>(),
-            second.split_whitespace().filter_map(|s| s.parse().ok()).collect::<Vec<i32>>()
-        )).fold(0, |acc, (x,y)| match x.iter().filter(|x| y.contains(x)).count() {
-            0 => acc,
-            count => (1 << count - 1) + acc
-        });
-
-    let mut points2: Vec<_> = fs::read_to_string("input.txt")
+    let mut points: Vec<_> = fs::read_to_string("input.txt")
         .expect("Something went wrong reading file!")
         .lines()
         .filter_map(|line| line
@@ -36,6 +21,8 @@ fn main() {
         }
     }
 
-    println!("{:?}", points1);
-    println!("{:?}", points2.iter().map(|(a, _)| a).sum::<usize>());
+    println!("Part 2 & 1: {:?}", points.iter().fold((0,0), |acc, &(x, y)| (acc.0 + x, match y {
+        0 => acc.1,
+        count => (1 << count - 1) + acc.1
+    })));
 }
