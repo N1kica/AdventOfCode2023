feea0a7958d40f99809dd0f3d7d8ce5fd22d4ffb
N1kica <68340084+N1kica@users.noreply.github.com>
2024-02-22 12:29:49 +0100

    Update Day4.rs

--- Day4.rs
+++ Day4.rs
@@ -1,31 +1,28 @@
 use std::fs;
 
-fn union_find(mut points: Vec<(i32, usize)>) -> Vec<(i32, usize)> {
-    for i in 0..points.len() {
-        for j in (i + 1)..(i + 1 + points[i].1).min(points.len()) {
-            points[j].0 += points[i].0;
-        }
-    }
-    points
-}
-
 fn main() {
-    let points: Vec<_> = fs::read_to_string("input.txt")
+    let mut cards: Vec<_> = fs::read_to_string("input.txt")
         .expect("Error!")
         .lines()
         .filter_map(|line| line
             .split_once(":")
             .and_then(|(_, second)| second.split_once("|"))
         )
-        .map(|(first, second)| (
-            first.split_whitespace().filter_map(|s| s.parse().ok()).collect::<Vec<i32>>(),
-            second.split_whitespace().filter_map(|s| s.parse().ok()).collect::<Vec<i32>>()
+        .map(|(winners, drawn)| (
+            winners.split_whitespace().filter_map(|s| s.parse().ok()).collect::<Vec<i32>>(),
+            drawn.split_whitespace().filter_map(|s| s.parse().ok()).collect::<Vec<i32>>()
         ))
-        .map(|(x, y)| (1, x.iter().filter(|x| y.contains(x)).count()))
+        .map(|(winners, drawn)| (1, winners.iter().filter(|winning| drawn.contains(winning)).count()))
         .collect();
 
-    println!("Part 1 & 2: {:?}", union_find(points).iter().fold((0,0), |acc, &(x, y)| (match y {
+    for i in 0..cards.len() {
+        for j in (i + 1)..(i + 1 + cards[i].1).min(cards.len()) {
+            cards[j].0 += cards[i].0;
+        }
+    }
+
+    println!("Part 1 & 2: {:?}", cards.iter().fold((0,0), |acc, &(copies, points)| (match points {
         0 => acc.0,
         count => (1 << count - 1) + acc.0
-    }, acc.1 + x)));
+    }, acc.1 + copies)));
 }
